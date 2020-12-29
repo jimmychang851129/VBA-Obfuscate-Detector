9224a6d18d2fb114f4374b14b15735d1b58a85bb
jimmy <jimmychang851129@gmail.com>
2020-12-30 03:34:07 +0800

    token renaming detection ver 1 done

--- TokenRename/tokenrename.py
+++ TokenRename/tokenrename.py
@@ -2,9 +2,8 @@ from pygments import highlight
 from pygments.lexers import VBScriptLexer
 from pygments.formatter import Formatter
 from pygments.token import Token
-import math, os, sys
+import math, os
 import argparse
-sys.path.append("../")
 
 charlist = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
 
@@ -24,19 +23,7 @@ def SimpleTokenParser(source_code: str) -> list:
     highlight(source_code, VBScriptLexer(), myFormatter)
     return myFormatter.token_list
 
-
-def Entropy(l):
-    cnt = 0
-    chardict = dict()
-    for c in charlist:
-        chardict[c] = 0
-    for s in l:
-        for c in s:
-            if c not in chardict:
-                print("[Entropy]: %c not in chardict"%(c))
-                continue
-            chardict[c]+= 1
-            cnt += 1
+def countEntropy(chardict,cnt):
     entropy = 0
     for key in chardict.keys():
         if chardict[key] != 0:
@@ -44,16 +31,63 @@ def Entropy(l):
             entropy -= p * math.log(p)
     return entropy
 
+# [All, Token.Name,Token.Function,Token.Literal.String.Double]
+def Entropy(l):
+    cnt = 0
+    EntroList = []
+    chardict = dict()
+    for c in charlist:
+        chardict[c] = 0
+    for s in l.keys():
+        for ele in l[s]:
+            for c in ele:
+                if c not in chardict:
+                    # print("[Entropy]: %c not in chardict"%(c))
+                    continue
+                chardict[c]+= 1
+                cnt += 1
+    EntroList.append(countEntropy(chardict,cnt))
+    tokenlist = [Token.Name,Token.Name.Function,Token.Literal.String.Double]
+    for token in tokenlist:
+        cnt = 0
+        chardict = dict()
+        for c in charlist:
+            chardict[c] = 0
+        if token in l:
+            for ele in l[token]:
+                for c in ele:
+                    if c not in chardict:
+                        # print("[Entropy]: %c not in chardict"%(c))
+                        continue
+                    chardict[c]+= 1
+                    cnt += 1
+            EntroList.append(countEntropy(chardict,cnt))
+        else:
+            EntroList.append(0)   
+    return EntroList
+
+# Token.Name.Variable: Null
 def getToken(code):
-    whitelist = set({Token.Literal.String.Double,Token.Name,Token.Name.Variable,Token.Name.Function})
+    whitelist = set({Token.Literal.String.Double,Token.Name,Token.Name.Function})
     tokenlist = SimpleTokenParser(code)
-    l = []
+    l = dict()
     for token in tokenlist:
         if token[0] in whitelist:
+            if token[0] not in l:
+                l[token[0]] = []
             if token[0] == Token.Literal.String.Double and len(token[1]) <= 4:
                 continue
-            l.append(token[1])
+            l[token[0]].append(token[1])
     return l
+
+def getVar(tokenlist):
+    varlist = []
+    for ele in tokenlist[Token.Name]:
+        if Token.Name.Function not in tokenlist or ele not in tokenlist[Token.Name.Function]:
+            varlist.append(ele)
+    tokenlist.pop(Token.Name,None)
+    tokenlist[Token.Name] = varlist
+    return tokenlist
 
 # string.double beware length
 # builtin may contains op such as strreverse...
@@ -64,18 +98,23 @@ def getEntropy(dirpath):
         inputfile = os.path.join(dirpath,file)
         code = open(inputfile,'r').read()
         tokenlist = getToken(code)
+        tokenlist = getVar(tokenlist)
         EntropyList.append(Entropy(tokenlist))
         filelist.append(file)
     return EntropyList,filelist
 
-def writeFile(filelist,EntropyList):
-    with open("entropy.csv",'w') as fw:
+def writeFile(filelist,EntropyList,outputdir):
+    filepath = os.path.join(outputdir,"entropy.csv")
+    with open(filepath,'w') as fw:
+        fw.write("Filename,Overall Entropy,Variable Name Entropy,Function Name Entropy,Strings Entropy\n")
         for i in range(len(EntropyList)):
-            fw.write(filelist[i]+","+str(EntropyList[i])+"\n")
+            out = ",".join([str(x) for x in EntropyList[i]])
+            fw.write(filelist[i]+","+out+"\n")
 
 def CalculateSingleEntropy(filepath):
     code = open(filepath,'r').read()
     tokenlist = getToken(code)
+    tokenlist = getVar(tokenlist)
     return Entropy(tokenlist)
 
 def ParseArg():
@@ -87,10 +126,19 @@ def ParseArg():
     args = parser.parse_args()
     return args
 
+def PrintResult(ret):
+    entropyName = ["Overall Entropy","Variable Name Entropy","Function Name Entropy","Strings Entropy"]
+    for i in range(len(ret)):
+        print("%s ... %f"%(entropyName[i],ret[i]))
+    if ret[0] > 3.5:
+        print("Token Renaming Detection ... True")
+    else:
+        print("Token Renaming Detection ... False")
+
 def main(args):
     if args.test == True:
         ret = CalculateSingleEntropy(args.file)
-        print("entropy = ",ret)
+        PrintResult(ret)
     else:
         entrolist, filelist = getEntropy(args.inputdir)
         writeFile(filelist,entrolist,args.outputdir)
