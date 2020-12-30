db23bd0da743b0319a073eccec6404bfc3d92267
tpainting <caleb860513@gmail.com>
2020-12-30 12:07:06 +0800

    chr() function call detection

--- VBParser.py
+++ VBParser.py
@@ -10,6 +10,8 @@ class MyFormatter(Formatter):
     def format(self, tokensource, outfile):
         for type, value in tokensource:
             if type != Token.Text.Whitespace:
+                if type == Token.Name and (value.lower() =='chrw' or value.lower() =='chrb'):
+                    type = Token.Name.Builtin
                 self.token_list.append((type, value))
 
 def SimpleTokenParser(source_code: str) -> list:
@@ -23,6 +25,8 @@ if __name__ == "__main__":
     code = '''
     Sub AAA():
         A = Array(1,2,3,4,5, Chr(123), ChrB(234), ChrW(3453))
+        cHr(123)
+        ChR(234)
     End Sub
     '''
     code = code.strip().split('\n')
